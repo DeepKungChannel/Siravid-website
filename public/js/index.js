e02f0821a242f986df1bead6cf45a93f3aee715a
deepkung1904 <ppoumppoum@gmail.com>
2022-02-17 21:12:11 +0700

    Siravid Thongsook portfolio

--- public/js/index.js
+++ public/js/index.js
@@ -8,15 +8,21 @@ nav_button.addEventListener('click', () => {
   bar.classList.toggle('show');
   x.classList.toggle('show');
   nav_link.classList.toggle('hide');
-
 })
 
+nav_link.addEventListener('click', () => {
+  console.log(! ('hide' in nav_link.classList))
+  if (! ('hide' in nav_link.classList)) {
+    bar.classList.add('show');
+    x.classList.remove('show');
+    nav_link.classList.add('hide');
+  }
+})
 
-
-for (var i = link.length - 1; i >= 0; i--) {
-  link[i].addEventListener('click', () => {
-    bar.classList.toggle('show');
-    x.classList.toggle('show');
-    nav_link.classList.toggle('hide'); 
-  })
-}
+// for (var i = link.length - 1; i >= 0; i--) {
+//   link[i].addEventListener('click', () => {
+//     bar.classList.toggle('show');
+//     x.classList.toggle('show');
+//     nav_link.classList.toggle('hide'); 
+//   })
+// }
